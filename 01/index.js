76c1dbe2561222d0b5fd9e613c07805ed43b8fad
Luanna <lua.varjao@gmail.com>
2023-08-20 10:46:29 -0300

    Resolução Ex 01

---
+++ 01/index.js
@@ -0,0 +1,4 @@
+const livros = ['Guerra e Paz', 'A Montanha Mágica', 'Cem Anos de Solidão', 'Dom Quixote', 'A Divina Comédia'];
+const nomeDoLivro = "Dom Quixote";
+
+console.log(`O livro ${nomeDoLivro} está na posição ${livros.indexOf(nomeDoLivro)}`)
