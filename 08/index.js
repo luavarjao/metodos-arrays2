310d23731fbe5c140eaa38b7be42097df89dcc29
Luanna <lua.varjao@gmail.com>
2023-08-20 15:02:12 -0300

    Resolução 08

---
+++ 08/index.js
@@ -0,0 +1,8 @@
+const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"]
+
+
+const novasFrutas = frutas.map((fruta, indice) =>{
+    return `${indice} - ${fruta.slice(0,1).toUpperCase()}${fruta.slice(1).toLowerCase()}`;
+})
+
+console.log(novasFrutas);
